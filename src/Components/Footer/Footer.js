ae6037a0d12e1d11c749bcdde5dba888b7dcd323
Atıf Şimşek <dev.atifsimsek@gmail.com>
2022-11-23 03:30:19 +0300

    footer changed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,33 +1,53 @@
 import styles from "./footer.module.scss"
 import imgLogo from "../../assets/img/logo-yazisiz-removebg-preview.png"
+import { MdLocationOn } from "react-icons/md"
+import { AiFillClockCircle } from "react-icons/ai"
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaMobileAlt, FaPaperPlane } from "react-icons/fa"
+
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.footerContainer}>
-        <nav className={styles.contact}>
-          <h3>İletişim</h3>
+        <div className={styles.hours}>
+          <h3>Açık Saatler</h3>
           <ul>
-            <li>Mail</li>
-            <li>Telefon</li>
-            <li>Adres</li>
+            <li>
+              <AiFillClockCircle />
+              <span>Hafta İçi 08:00 - 17:00</span>
+            </li>
+            <li>
+              <AiFillClockCircle />
+              <span>Hafta Sonu 08:00 - 12:00</span>
+            </li>
           </ul>
-        </nav>
+        </div>
         <div className={styles.logo}>
           <img src={imgLogo} alt="" />
-          <h3>Erdem Yüksel Metal</h3>
-        </div>
-        <nav className={styles.social}>
-          <h3>Sosyal Medya</h3>
           <ul>
-            <li>Facebook</li>
-            <li>İnstagram</li>
-            <li>Twitter</li>
+            <li><FaFacebook /></li>
+            <li><FaInstagram /></li>
+            <li><FaTwitter /></li>
+            <li><FaLinkedin /></li>
           </ul>
-        </nav>
-      </div>
-      <div className={styles.copy}>
-        &copy;2022 Erdem Yüksel Metal
+        </div>
+        <div className={styles.contact}>
+          <h3>İletişim</h3>
+          <ul>
+            <li>
+              <span>Adres</span>
+              <MdLocationOn />
+            </li>
+            <li>
+              <span>example@gmail.com</span>
+              <FaPaperPlane />
+            </li>
+            <li>
+              <span>0 550 000 00 00</span>
+              <FaMobileAlt />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
